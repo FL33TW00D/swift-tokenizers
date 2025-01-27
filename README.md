9f20c6f97d1892f7ebbf9cdc49901653637a0a18
FL33TW00D <FL33TW00D@users.noreply.github.com>
2025-01-27 16:33:02 +0000

    chore: experiments

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # swift-tokenizers
+
+Experiments in directly utilizing the Rust `tokenizers` library in Swift.
+
+Bindings are generated using `swift-bridge`.
+
+```
+./build-swiftc-links-rust.sh
+./main
+
+Tokens:
+101
+7592
+2088
+999
+102
+```
--- main.swift
+++ main.swift
@@ -1,5 +1,5 @@
 if let tokenizer = Tokenizer.from_pretrained("bert-base-uncased") {
-    let tokens = tokenizer.encode("Hello world!", true)
+    let tokens = tokenizer.encode_to_ids("Hello world!", true)
     for token in tokens {
         print(token)
     }
