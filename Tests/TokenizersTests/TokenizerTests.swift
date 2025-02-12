f6aa308273253fdba9bf4b5382ccd98193294c53
FL33TW00D <FL33TW00D@users.noreply.github.com>
2025-02-12 14:38:02 +0000

    chore: NLLB

--- Tests/TokenizersTests/TokenizerTests.swift
+++ Tests/TokenizersTests/TokenizerTests.swift
@@ -3,6 +3,7 @@ import XCTest
 
 final class TokenizerTests: XCTestCase {
     func testBasicTokenization() throws {
+        print("===== testBasicTokenization =====")
         let tokenizer = try Tokenizer.fromPretrained(name: "bert-base-uncased")
         
         let input = "Hello, world!"
@@ -20,6 +21,7 @@ final class TokenizerTests: XCTestCase {
 
     //https://github.com/huggingface/swift-transformers/issues/96
     func testMatchesPythonTokenization() async throws {
+        print("===== testMatchesPythonTokenization =====")
         let tokenizer = try Tokenizer.fromPretrained(name: "mlx-community/Phi-3-mini-4k-instruct-4bit-no-q-embed")
         let input = " Hi"
 
@@ -31,6 +33,31 @@ final class TokenizerTests: XCTestCase {
         print(encoding.tokens)
         print(encoding.ids)
 
+        let decoded = try tokenizer.decode(encoding.ids)
+        print(decoded)
+        XCTAssertFalse(decoded.isEmpty)
+    }
+
+    //https://github.com/huggingface/swift-transformers/issues/99
+    func testE5() async throws {
+        print("===== testE5 =====")
+        let tokenizer = try Tokenizer.fromPretrained(name: "intfloat/multilingual-e5-small")
+        let encoding = try tokenizer.encode("query: how much protein should a female eat")
+        print(encoding.ids)
+        
+        let decoded = try tokenizer.decode(encoding.ids)
+        print(decoded)
+        XCTAssertEqual(encoding.ids, [0, 41, 1294, 12, 3642, 5045, 21308, 5608, 10, 117776, 73203, 2])
+    }
+
+    //https://github.com/huggingface/swift-transformers/issues/107
+    func testNLLBTokenizer() async throws {
+        print("===== testNLLBTokenizer =====")
+        let tokenizer = try Tokenizer.fromPretrained(name: "facebook/nllb-200-distilled-600M")
+        let encoding = try tokenizer.encode("how much wood could a woodchuck chuck?")
+        XCTAssertFalse(encoding.tokens.isEmpty)
+        print(encoding.ids)
+        
         let decoded = try tokenizer.decode(encoding.ids)
         print(decoded)
         XCTAssertFalse(decoded.isEmpty)
